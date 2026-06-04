d4cca34432132615da9a5abaff82480b6029268a
Estrup <sen@elbek-vejrup.dk>
2026-06-04 18:04:57 +0200

    refactor: remove marionette_flutter and marionette_mcp dependencies

--- lib/app/bootstrap.dart
+++ lib/app/bootstrap.dart
@@ -1,15 +1,9 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:marionette_flutter/marionette_flutter.dart';
 
 import 'package:tree_launcher/app/app.dart';
 
 Future<void> bootstrap() async {
-  if (kDebugMode) {
-    MarionetteBinding.ensureInitialized();
-  } else {
-    WidgetsFlutterBinding.ensureInitialized();
-  }
+  WidgetsFlutterBinding.ensureInitialized();
 
   runApp(const TreeLauncherApp());
 }
